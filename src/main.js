0babd3ebcb40e9899fbebe7a9e6bb723087efd27
jinshiyi11 <49662990@qq.com>
2017-07-13 23:53:39 +0800

    1.添加feed list

--- src/main.js
+++ src/main.js
@@ -2,20 +2,21 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import FastClick from 'fastclick'
-import VueRouter from 'vue-router'
+// import VueRouter from 'vue-router'
+import router from './router/index.js'
 import App from './App'
-import Home from './components/HelloFromVux'
+// import Home from './components/HelloFromVux'
 
-Vue.use(VueRouter)
+// Vue.use(VueRouter)
 
-const routes = [{
-  path: '/',
-  component: Home
-}]
+// const routes = [{
+//   path: '/',
+//   component: Home
+// }]
 
-const router = new VueRouter({
-  routes
-})
+// const router = new VueRouter({
+//   routes
+// })
 
 FastClick.attach(document.body)
 
